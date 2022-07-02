f7d982ed5cb67a243113ae0fa74f762be6391946
Neil Ranada <neil.ranada@gmail.com>
2022-07-01 19:08:51 -0700

    Add table style

--- view/fund-react-app/src/App.js
+++ view/fund-react-app/src/App.js
@@ -5,9 +5,9 @@ function App() {
   let amountLeftToInvest = 10000000000
 
   return (
-    <div className="App">
+    <div className="App-content-container">
       <h1>{pageViewTitle}</h1>
-      <h2>$ {amountLeftToInvest} left to invest</h2>
+      <h2>$ {amountLeftToInvest} <span style={{fontSize: "16px", fontWeight: 400}}>left to invest</span></h2>
       <input type="text"></input>
       <button>Add Funds</button>
       <p>Sort by</p>
@@ -15,8 +15,8 @@ function App() {
         <option value="name">Investment name</option>
         <option value="name">Amount invested</option>
       </select>
-      <table>
-          <thead>
+      <table className="App-table">
+          <thead className="App-table-headers">
             <tr>
               <th>Investment name</th>
               <th>Amount invested</th>
@@ -30,6 +30,10 @@ function App() {
             <tr>
               <td>Fund 2</td>
               <td>$2000</td>
+            </tr>
+             <tr>
+              <td>Fund 3</td>
+              <td>$10000</td>
             </tr>
           </tbody>
       </table>
