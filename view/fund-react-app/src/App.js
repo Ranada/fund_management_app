3e11158bfa1a79ed08793ed0f379a9a2927efc5f
Neil Ranada <neil.ranada@gmail.com>
2022-07-01 18:41:57 -0700

    first commit for static data for funds dashboard view

--- view/fund-react-app/src/App.js
+++ view/fund-react-app/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let pageViewTitle = "Funds Dashboard"
+  let amountLeftToInvest = 10000000000
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{pageViewTitle}</h1>
+      <h2>$ {amountLeftToInvest} left to invest</h2>
+      <input type="text"></input>
+      <button>Add Funds</button>
+      <p>Sort by</p>
+      <select>
+        <option value="name">Investment name</option>
+        <option value="name">Amount invested</option>
+      </select>
+      <table>
+          <thead>
+            <tr>
+              <th>Investment name</th>
+              <th>Amount invested</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Fund 1</td>
+              <td>$1000</td>
+            </tr>
+            <tr>
+              <td>Fund 2</td>
+              <td>$2000</td>
+            </tr>
+          </tbody>
+      </table>
     </div>
   );
 }
