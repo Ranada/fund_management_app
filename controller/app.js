a7174de098ddf5f66c8f1db41e50ed58b0220a6f
Scully_E <ethanscully1@gmail.com>
2022-07-08 12:03:48 +0800

    added mySQL database pulling for backend

--- controller/app.js
+++ controller/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const investment_table_router = require("./database/investment_table_routes")
+const investment_table_router = require("../database/investment_table_routes")
 
 app.use(express.json());
 app.use("/investments", investment_table_router);
